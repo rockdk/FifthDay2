c541dc0fe72775bbab033c1e48cca5c1ddbea263
rockdk <deepak875618@gmail.com>
2018-06-25 21:58:44 +0530

    Initial commit

---
+++ app/src/main/java/com/champ/web/fifthday2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.champ.web.fifthday2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=findViewById(R.id.editText);
+        e2=findViewById(R.id.editText2);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=e1.getText().toString();
+                String s2=e2.getText().toString();
+
+                Toast.makeText(getApplicationContext(),
+                        ""+s1+s2,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+    }
+}
